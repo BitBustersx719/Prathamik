6d3385e9d35a59a0ba740863597a25fb8895a8dd
Priyanshu <priyanshurouth@gmail.com>
2023-06-06 22:45:14 +0530

    API Intregrated

--- server/configApi/whiteboard.py
+++ server/configApi/whiteboard.py
@@ -1,22 +1,38 @@
 from flask import Flask, request, jsonify, send_file
-from werkzeug.utils import secure_filename
-import pytesseract
+from google.cloud import vision_v1
 from PIL import Image
-import io
 import requests
-AllOWED_EXTENSION= (['text','png','jpg','jpeg','gif','pdf'])
+import os
+import io
 app = Flask(__name__)
 
+# Set the environment variable for the service account key file
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:\\Users\\HP\\Desktop\\Projects\\Prathamik\\myenv\\ocr-vision-388911-0dcf28a813d9 (1).json"
 @app.route('/ocr', methods=['POST'])
 def ocr():
     if 'image' not in request.files:
         return jsonify({'error': 'No image uploaded'})
 
-    image = request.files['image']
-    img = Image.open(image)
-    text = pytesseract.image_to_string(image=img)
+    # Initialize the client
+    client = vision_v1.ImageAnnotatorClient()
 
-    return jsonify({'text': text})
+    # Read the image file
+    image = request.files['image'].read()
+
+    # Create an image object
+    image_obj = vision_v1.Image(content=image)
+
+    # Perform OCR
+    response = client.text_detection(image=image_obj)
+    texts = response.text_annotations
+
+    # Extract the detected text
+    if texts:
+        extracted_text = texts[0].description
+    else:
+        extracted_text = ""
+
+    return jsonify({'text': extracted_text})
 
 @app.route('/test', methods=['POST'])
 def test():
@@ -27,18 +43,6 @@ def test():
     img_bytes.seek(0)
 
     return send_file(img_bytes, mimetype='image/png')
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    url = "http://127.0.0.1:5000/test"  # Replace with your API endpoint URL
-
-    payload = {
-        "file": request.files['image']
-    }
-
-    response = requests.post(url, files=payload)
-
-    return response.text, response.status_code
 
 if __name__ == '__main__':
     app.run(debug=True)
