4eaff2c9fa9baf8a4677e31cd1919510bc826fca
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-06-06 21:35:48 +0530

    submit

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import 'font-awesome/css/font-awesome.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    {/* <App/> */}
     <Navbar/>
     <Landing/>
   </React.StrictMode>
