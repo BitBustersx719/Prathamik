5e15bfb7cf9d6c9ea4972e196ffaeaa02486fa6b
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-06-08 22:29:58 +0530

    about section scroll animation added

--- client/src/About.js
+++ client/src/About.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef} from 'react';
 import './About.css';
 import './index.css';
 import Community from './images/community.png';
@@ -6,37 +6,62 @@ import Experience from './images/experience.png';
 import Expert from './images/expert.png';
 import Technology from './images/technology.png';
 
-function Navbar() 
+function About() 
 {
-  const [shouldRender,setShouldRender]=useState(false);
-  useEffect(()=>
-  {
-    const timer=setTimeout(()=>
-      {
-        setShouldRender(true);
-      }, 100);
-      return () => clearTimeout(timer); 
-  },[]);
+    const headingRef = useRef(null);
+    const communityRef = useRef(null);
+    const technologyRef = useRef(null);
+    const expertRef = useRef(null);
+    const experienceRef = useRef(null);
 
+    useEffect(() => 
+    {
+        const observer = new IntersectionObserver((entries, observer) => 
+        {
+            console.log("intersection done",entries);
+            entries.forEach(entry => 
+                {
+                    if (entry.isIntersecting) 
+                    {
+                      entry.target.classList.add('showAbout');
+                    }
+                });
+        }, {threshold: 0.5});
+
+        if (headingRef.current) observer.observe(headingRef.current);
+        if (communityRef.current) observer.observe(communityRef.current);
+        if (technologyRef.current) observer.observe(technologyRef.current);
+        if (expertRef.current) observer.observe(expertRef.current);
+        if (experienceRef.current) observer.observe(experienceRef.current);
+
+        return () => 
+        {
+            if (headingRef.current) observer.unobserve(headingRef.current);
+            if (communityRef.current) observer.unobserve(communityRef.current);
+            if (technologyRef.current) observer.unobserve(technologyRef.current);
+            if (expertRef.current) observer.unobserve(expertRef.current);
+            if (experienceRef.current) observer.unobserve(experienceRef.current);
+        };
+    }, []);
 
   return (
     <div>
-      {shouldRender && <div className='about_container'>
-        <div className='about_heading'>
+      <div className='about_container'>
+        <div className='about_heading' ref={headingRef}>
             <h1>About Us</h1>
             <div className='about_line'></div>
         </div>
         
         <div className='about_blocks'>
-            <div className='community'>
+            <div className='community' ref={communityRef}>
                 <img src={Community} />
                 <div className='description'>
                     <h2>Who We Serve</h2>
-                    <p>At Prathamik, we are dedicated to providing accessible education for individuals of all backgrounds and ages. Our platform serves a diverse community of learners who are passionate about personal growth and expanding their knowledge. We offer a wide range of courses tailored to meet the needs of different learners. Whether you're a.</p>
+                    <p>At Prathamik, we are dedicated to providing accessible education for individuals of all backgrounds and ages. Our platform serves a diverse community of learners who are passionate about personal growth and expanding their knowledge. We offer a wide range of courses tailored to meet the needs of different learners.</p>
                 </div>
             </div>
 
-            <div className='technology'>
+            <div className='technology' ref={technologyRef}>
                 <div className='description'>
                     <h2>Cutting-Edge Technology</h2>
                     <p>At the heart of our e-learning platform lies cutting-edge technology that drives an innovative and immersive learning experience. We leverage the latest advancements in educational technology to deliver dynamic and interactive course content. From virtual simulations and augmented reality to adaptive learning algorithms, we harness.</p>
@@ -44,32 +69,31 @@ function Navbar()
                 <img src={Technology} />
             </div>
         </div>
+      </div>
 
-      </div>}
-      {shouldRender && <div className='about_container'>
+
+      <div className='about_container'>
         <div className='about_blocks'>
-
-            <div className='expert'>
+            <div className='expert' ref={expertRef}>
                 <img src={Expert} />
                 <div className='description'>
                     <h2>Our Team of Experts</h2>
-                    <p>We take pride in our team of experts who are passionate about education and dedicated to providing the highest quality learning experience. Our instructors are industry professionals, subject matter experts, and experienced educators who bring their wealth of knowledge and practical insights into the courses they teach. With their guidance.</p>
+                    <p>We take pride in our team of experts who are passionate about education and dedicated to providing the highest quality learning experience. Our instructors are industry professionals, subject matter experts, and experienced educators who bring their wealth of knowledge and practical insights into the courses they teach.</p>
                 </div>
             </div>
 
-            <div className='experience'>
+            <div className='experience' ref={experienceRef}>
                 <div className='description'>
                     <h2>Seamless User Experience</h2>
-                    <p>At [Website Name], we are committed to providing a seamless learning experience from start to finish. Our user-friendly platform is designed with intuitive navigation and clear instructions, making it easy for you to navigate through courses, access resources, and track your progress. We understand that your time is valuable, so we strive to.</p>
+                    <p>At Prathamik, we are committed to providing a seamless learning experience from start to finish. Our user-friendly platform is designed with intuitive navigation and clear instructions, making it easy for you to navigate through courses, access resources, and track your progress. We understand that your time is valuable, so we strive to.</p>
                 </div>
                 <img src={Experience} />
             </div>
-
         </div>
-      </div>}
+      </div>
+
     </div>
   );
 }
 
-export default Navbar;
-
+export default About;
