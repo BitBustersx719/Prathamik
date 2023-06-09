ddc384a2e147d728959d15533b226a66410a5a58
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-06-09 14:45:52 +0530

    about animation enhanced

--- client/src/About.js
+++ client/src/About.js
@@ -1,54 +1,100 @@
 import React, { useEffect, useState, useRef} from 'react';
 import './About.css';
 import './index.css';
-import Community from './images/community.png';
-import Experience from './images/experience.png';
-import Expert from './images/expert.png';
-import Technology from './images/technology.png';
+import Community from './images/community-nobg.png';
+import Experience from './images/experience-nobg.png';
+import Expert from './images/expert-nobg.png';
+import Technology from './images/technology-nobg.png';
 
 function About() 
 {
     const headingRef = useRef(null);
-    const communityRef = useRef(null);
-    const technologyRef = useRef(null);
-    const expertRef = useRef(null);
-    const experienceRef = useRef(null);
+    const communityImageRef = useRef(null);
+    const technologyImageRef = useRef(null);
+    const expertImageRef = useRef(null);
+    const experienceImageRef = useRef(null);
+    const communityDescriptionRef = useRef(null);
+    const technologyDescriptionRef = useRef(null);
+    const expertDescriptionRef = useRef(null);
+    const experienceDescriptionRef = useRef(null);
+
+    
 
     useEffect(() => 
     {
-        const options={
-          root: null,
-          rootMargin: "0px",
-          threshold: 0.7
-        };
         const observer = new IntersectionObserver((entries, observer) => 
         {
             entries.forEach(entry => 
                 {
-                  
                   if (entry.isIntersecting) 
                     {
-                      console.log("intersection done",entries);
-                      entry.target.classList.add('showAbout');
+                      entry.target.classList.add('showAboutHeading');
                     }
                 });
-        }, options);
+        }, {threshold: 0.6});
 
         if (headingRef.current) observer.observe(headingRef.current);
-        if (communityRef.current) observer.observe(communityRef.current);
-        if (technologyRef.current) observer.observe(technologyRef.current);
-        if (expertRef.current) observer.observe(expertRef.current);
-        if (experienceRef.current) observer.observe(experienceRef.current);
 
         return () => 
         {
             if (headingRef.current) observer.unobserve(headingRef.current);
-            if (communityRef.current) observer.unobserve(communityRef.current);
-            if (technologyRef.current) observer.unobserve(technologyRef.current);
-            if (expertRef.current) observer.unobserve(expertRef.current);
-            if (experienceRef.current) observer.unobserve(experienceRef.current);
         };
     }, []);
+
+    useEffect(() => 
+    {
+        const observer = new IntersectionObserver((entries, observer) => 
+        {
+            entries.forEach(entry => 
+                {
+                  if (entry.isIntersecting) 
+                    {
+                      entry.target.classList.add('animateLeft');
+                    }
+                });
+        }, {threshold: 0.8});
+
+        if (communityDescriptionRef.current) observer.observe(communityDescriptionRef.current);
+        if (expertDescriptionRef.current) observer.observe(expertDescriptionRef.current);
+        if (technologyImageRef.current) observer.observe(technologyImageRef.current);
+        if (experienceImageRef.current) observer.observe(experienceImageRef.current);
+
+        return () => 
+        {
+            if (communityDescriptionRef.current) observer.unobserve(communityDescriptionRef.current);
+            if (expertDescriptionRef.current) observer.unobserve(expertDescriptionRef.current);
+            if (technologyImageRef.current) observer.unobserve(technologyImageRef.current);
+            if (experienceImageRef.current) observer.unobserve(experienceImageRef.current);
+        };
+    }, []);
+
+    useEffect(() => 
+    {
+        const observer = new IntersectionObserver((entries, observer) => 
+        {
+            entries.forEach(entry => 
+                {
+                  if (entry.isIntersecting) 
+                    {
+                      entry.target.classList.add('animateRight');
+                    }
+                });
+        }, {threshold: 0.8});
+
+        if (communityImageRef.current) observer.observe(communityImageRef.current);
+        if (expertImageRef.current) observer.observe(expertImageRef.current);
+        if (technologyDescriptionRef.current) observer.observe(technologyDescriptionRef.current);
+        if (experienceDescriptionRef.current) observer.observe(experienceDescriptionRef.current);
+
+        return () => 
+        {
+            if (communityImageRef.current) observer.unobserve(communityImageRef.current);
+            if (expertImageRef.current) observer.unobserve(expertImageRef.current);
+            if (technologyDescriptionRef.current) observer.unobserve(technologyDescriptionRef.current);
+            if (experienceDescriptionRef.current) observer.unobserve(experienceDescriptionRef.current);
+        };
+    }, []);
+
 
   return (
     <div>
@@ -59,36 +105,36 @@ function About()
         </div>
         
         <div className='about_blocks'>
-            <div ref={communityRef} className='community'>
-                <img src={Community} />
-                <div className='description'>
+            <div className='community'>
+                <img src={Community} ref={communityImageRef}/>
+                <div className='community_description' ref={communityDescriptionRef}>
                     <h2>Who We Serve</h2>
-                    <p>At Prathamik, we are dedicated to providing accessible education for individuals of all backgrounds and ages. Our platform serves a diverse community of learners who are passionate about personal growth and expanding their knowledge. We offer a wide range of courses tailored to meet the needs of different learners.</p>
+                    <p>We take pride in our team of experts who are passionate about education and dedicated to providing the highest quality learning experience. Our instructors are industry professionals, subject matter experts, and experienced educators who bring their wealth of knowledge and practical insights into the.</p>
                 </div>
             </div>
 
-            <div ref={technologyRef} className='technology'>
-                <div className='description'>
+            <div className='technology'>
+                <div className='technology_description' ref={technologyDescriptionRef}>
                     <h2>Cutting-Edge Technology</h2>
                     <p>At the heart of our e-learning platform lies cutting-edge technology that drives an innovative and immersive learning experience. We leverage the latest advancements in educational technology to deliver dynamic and interactive course content. From virtual simulations and augmented reality to adaptive learning algorithms, we harness.</p>
                 </div>
-                <img src={Technology} />
+                <img src={Technology} ref={technologyImageRef}/>
             </div>
 
-            <div ref={expertRef} className='expert'>
-                <img src={Expert} />
-                <div className='description'>
+            <div className='expert'>
+                <img src={Expert} ref={expertImageRef}/>
+                <div className='expert_description' ref={expertDescriptionRef}>
                     <h2>Our Team of Experts</h2>
                     <p>We take pride in our team of experts who are passionate about education and dedicated to providing the highest quality learning experience. Our instructors are industry professionals, subject matter experts, and experienced educators who bring their wealth of knowledge and practical insights into the courses they teach.</p>
                 </div>
             </div>
 
-            <div ref={experienceRef} className='experience'>
-                <div className='description'>
+            <div className='experience'>
+                <div className='experience_description' ref={experienceDescriptionRef}>
                     <h2>Seamless User Experience</h2>
                     <p>At Prathamik, we are committed to providing a seamless learning experience from start to finish. Our user-friendly platform is designed with intuitive navigation and clear instructions, making it easy for you to navigate through courses, access resources, and track your progress. We understand that your time is valuable, so we strive to.</p>
                 </div>
-                <img src={Experience} />
+                <img src={Experience} ref={experienceImageRef}/>
             </div>
         </div>
 
