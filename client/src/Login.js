247b5039ecc0e0e8c1d0352f638a30f4de22aa2d
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-06-18 20:25:24 +0530

    basic login signup backend done

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,17 +1,71 @@
 import React from 'react';
+import { useState } from 'react';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import './Login.css';
 
 function Login()
 {
+    const [data,setData]=useState({
+        email:'',
+        password:'',
+    });
+
+    const [error,setError]=useState('');
+
+    const handleChange=({currentTarget:input})=>{
+        setData({...data,[input.name]:input.value}); 
+    }
+
+    const handleSubmit= async  (e)=>{
+        e.preventDefault();
+        try{
+            const url='http://localhost:3000/login';
+            const response=await axios.post(url,data);
+            const res=response.data;
+            localStorage.setItem('user', JSON.stringify(res));
+            // console.log(res);
+            console.log(res.data);
+            console.log(res.message);
+            
+        }catch(error){
+            if (error.response && error.response.status>=400 && error.response.status<500)
+            {
+                setError(error.response.data.message);
+                console.log(error.response.data.message);
+            }
+        } 
+    }
+
     return(
-        <div className='login-container'>
-            <form>
-                <label htmlFor='email'>Email:</label><br></br>
-                <input type="email" id='email' name="email" required /><br></br>
+        <div className='signup-container'>
+            <form onSubmit={handleSubmit}>
 
-                <label htmlFor='password'>Password:</label><br></br>
-                <input type="password" id='password' name="password" required /><br></br>
+                <input
+                    type="email"
+                    placeholder='Email'
+                    name="email"
+                    value={data.email}
+                    onChange={handleChange}
+                    required
+                />
 
+                <br></br>
+
+                <input
+                    type="password"
+                    placeholder='Password'
+                    name="password"
+                    value={data.password}
+                    onChange={handleChange}
+                    required
+                />
+
+                <br></br>
+
+                {error && <div className='error'>
+                    {error}
+                </div>}
                 <button type="submit">Login</button>
             </form>
         </div>
@@ -19,3 +73,4 @@ function Login()
 }
 
 export default Login;
+
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,23 +1,91 @@
 import React from 'react';
+import { useState } from 'react';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import './Signup.css';
 
 function Signup()
 {
+    const [data,setData]=useState({
+        name:'',
+        email:'',
+        password:'',
+        confirm_password:''
+    });
+
+    const [error,setError]=useState('');
+    const navigate=useNavigate();
+
+    const handleChange=({currentTarget:input})=>{
+        setData({...data,[input.name]:input.value}); 
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const url = 'http://localhost:3000/signup';
+            const response = await axios.post(url, data);
+            //   const res=response.data;
+            //   navigate('/login');
+            console.log(response.data.message);
+            setError('');
+        } catch (error) {
+            if (error.response && error.response.status >= 400 && error.response.status < 500) {
+                setError(error.response.data.message);
+          }
+        }
+    };
+      
     return(
         <div className='signup-container'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor='name'>Full Name:</label><br></br>
-                <input type="text" id='name' name="name" required/><br></br>
+                <input
+                    type="text"
+                    placeholder='Name'
+                    name="name"
+                    value={data.name}
+                    onChange={handleChange}
+                    required
+                />
 
-                <label htmlFor='email'>Email:</label><br></br>
-                <input type="email" id='email' name="email" required/><br></br>
+                <br></br>
 
-                <label htmlFor='password'>Password:</label><br></br>
-                <input type="password" id='password' name="password" required/><br></br>
+                <input
+                    type="email"
+                    placeholder='Email'
+                    name="email"
+                    value={data.email}
+                    onChange={handleChange}
+                    required
+                />
 
-                <label htmlFor='confirm_password'>Confirm Password:</label><br></br>
-                <input type="password" id='confirm_password' name="confirm_password" required/><br></br>
+                <br></br>
 
+                <input
+                    type="password"
+                    placeholder='Password'
+                    name="password"
+                    value={data.password}
+                    onChange={handleChange}
+                    required
+                />
+
+                <br></br>
+
+                <input
+                    type="password"
+                    placeholder='Confirm Password'
+                    name="confirm_password"
+                    value={data.confirm_password}
+                    onChange={handleChange}
+                    // required
+                />
+
+                <br></br>
+                {error && <div className='error'>
+                    {error}
+                </div>}
                 <button type="submit">Register</button>
             </form>
         </div>
@@ -25,3 +93,5 @@ function Signup()
 }
 
 export default Signup;
+
+
