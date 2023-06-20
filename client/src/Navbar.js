ad10d9d3e8443a0db1c945c924b417ae55a0a276
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-06-21 03:48:45 +0530

    profile show on login and logout option done

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -5,6 +5,7 @@ import './index.css';
 
 function Navbar()
 {
+  
   const [navbarShadow, setNavbarShadow] = useState(false);
   const [navTabscolor, setNavTabsColor]= useState(false);
   const [loginSignupColor, setLoginSignupColor]= useState(false);
@@ -15,7 +16,8 @@ function Navbar()
       setLoginSignupColor(true);
       setNavbarShadow(true);
     }
-    else {
+    else 
+    {
       setNavTabsColor(false);
       setLoginSignupColor(false);
       setNavbarShadow(false);
@@ -23,29 +25,79 @@ function Navbar()
   }
   window.addEventListener('scroll', showShadow);
 
-  return (
-    <div>
-      <nav className={navbarShadow ? 'transparency' : 'navbar'}>
-        <a href='http://localhost:3000/' className='logo-container'>
-            <div className='logo'>  </div>
-            <h1>Prathamik</h1>
-        </a>
+  
+  const [profileDetailsShow, setProfileDetailsShow]=useState(false);
 
-        <ul className={navTabscolor?'navigations-tabs nav-tabs-color':'navigations-tabs nav-tabs-white'}>
-          <li><a href="/">HOME</a></li>
-          <Link className='li' to="/platform">Platform</Link>
-          <Link className='li' to="/stream">Stream</Link>
-          <Link className='li' to="/streamjoin">StreamJoin</Link>
-          <li>
-            <div className='login-signup-container'>
-              <Link to="/login" className={loginSignupColor?'login login-color' : 'login login-white'}>LOG IN</Link>
-              <Link to="/signup" className={loginSignupColor?'signup signup-color' : 'signup signup-white'}>SIGN UP</Link>
-            </div>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+  function handleProfileClick()
+  {
+    if (profileDetailsShow)
+      setProfileDetailsShow(false);
+    else 
+      setProfileDetailsShow(true);
+  }
+  
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const handleLogout = () => 
+  {
+    // window.location.reload();
+    localStorage.removeItem('user');
+  };
+
+
+  if (user)
+  {
+    return (
+      <div>
+        <nav className={navbarShadow ? 'transparency' : 'navbar'}>
+          <a href='http://localhost:3000/' className='logo-container'>
+              <div className='logo'>  </div>
+              <h1>Prathamik</h1>
+          </a>
+
+          <ul className='navigations-tabs'>
+            <li><a href="/">HOME</a></li>
+            <Link className='li' to="/platform">PLATFORM</Link>
+            <Link className='li' to="/stream">STREAM</Link>
+            <Link className='li' to="/streamjoin">STREAMJOIN</Link>
+            <li className='navbar_profile' onClick={handleProfileClick}>
+              {/* <img src={User}/> */}
+              <div className='user_img'>{user.data.name}</div>
+              <span><i class="fa-solid fa-angle-down"></i></span>
+              {profileDetailsShow && <ul>
+                <li>Edit</li>
+                <li onClick={handleLogout}>Log out</li>
+              </ul>}
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+  else
+  {
+    return (
+      <div>
+        <nav className={navbarShadow ? 'transparency' : 'navbar'}>
+          <a href='http://localhost:3000/' className='logo-container'>
+              <div className='logo'>  </div>
+              <h1>Prathamik</h1>
+          </a>
+
+          <ul className='navigations-tabs'>
+            <li><a href="/">HOME</a></li>
+            <Link className='li' to="/platform">PLATFORM</Link>
+            <Link className='li' to="/stream">STREAM</Link>
+            <Link className='li' to="/streamjoin">STREAMJOIN</Link>
+            <li className='login-signup-container'>
+                <Link to="/login" className='login'>LOG IN</Link>
+                <Link to="/signup" className='signup'>SIGN UP</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
