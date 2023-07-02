9675e95fdca8d32439846b5984de1dbc8c72e1b2
Niladri <rickyrickado14@gmail.com>
2023-07-02 13:27:54 +0530

    updated stream

--- client/src/Stream.js
+++ client/src/Stream.js
@@ -4,21 +4,34 @@ import './Stream.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMicrophone, faVideo, faSquarePlus, faMicrophoneSlash, faVideoSlash } from '@fortawesome/free-solid-svg-icons';
 import io from 'socket.io-client';
-const agoraAppId = "3ec3dc68e69e4b539553415e34ce7b03"; // Replace with your Agora App ID
-const signalingServerUrl = 'http://localhost:3000'; // Replace with the URL of your signaling server
+
+const agoraAppId = "3ec3dc68e69e4b539553415e34ce7b03";
+const signalingServerUrl = 'http://localhost:3000';
+
 const Stream = () => {
   const remoteVideoRef = useRef(null);
   const screenShareRef = useRef(null);
-  const localStream = useRef(null);
+  const localStream = useRef({
+    cameraTrack: null,
+    screenTrack: null
+  });
   const socketRef = useRef(null);
-  const [streamStarted, setStreamStarted] = useState(false);
   const [screenShareStarted, setScreenShareStarted] = useState(false);
   const [audioStarted, setAudioStarted] = useState(false);
   const [videoStarted, setVideoStarted] = useState(false);
 
   const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
 
-  useEffect( async () => {
+  useEffect(() => {
+    connectToSignalingServer();
+    return () => {
+      if (socketRef.current) {
+        socketRef.current.disconnect();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
     client.on('user-published', async (user, mediaType) => {
       if (mediaType === 'video') {
         await client.subscribe(user, mediaType);
@@ -43,104 +56,90 @@ const Stream = () => {
       }
     });
 
-    await client.join(agoraAppId, 'stream', null);
-  }, []);
+    try {
+      console.log('Joining channel');
+      client.join(agoraAppId, 'stream', null);
+    } catch (error) {
+      console.log('Error joining Agora channel:', error);
+    }
 
-  useEffect(() => {
-    connectToSignalingServer();
     return () => {
       stopAgoraStream();
     };
   }, []);
 
   const startVideo = async () => {
-    if (screenShareStarted)
-      localStream.current.screenTrack.close();
-    setVideoStarted(true);
-    // const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
-    // await client.join(agoraAppId, 'stream', null);
+    if (screenShareStarted) {
+      stopScreenShare();
+    }
 
-    const cameraTrack = await AgoraRTC.createCameraVideoTrack();
-    // await client.publish(cameraTrack);
+    if (!videoStarted) {
+      setVideoStarted(true);
 
-    cameraTrack.play('local-video');
+      const cameraTrack = await AgoraRTC.createCameraVideoTrack();
+      await client.publish(cameraTrack);
 
-    localStream.current = { cameraTrack };
+      cameraTrack.play('local-video');
 
-    // client.on('user-published', async (user, mediaType) => {
-    //   if (mediaType === 'video') {
-    //     await client.subscribe(user, mediaType);
-    //     const remoteVideoTrack = user.videoTrack;
-    //     remoteVideoTrack.play(remoteVideoRef.current);
-    //   }
-    // });
+      localStream.current.cameraTrack = cameraTrack;
+    }
+  };
+
+  const startScreenShare = async () => {
+    if (videoStarted) {
+      stopVideo();
+    }
+
+    if (!screenShareStarted) {
+      setScreenShareStarted(true);
+
+      const screenTrack = await AgoraRTC.createScreenVideoTrack();
+      await client.publish(screenTrack);
+
+      screenTrack.play('local-video');
+
+      localStream.current.screenTrack = screenTrack;
+    }
+  };
+
+  const startAudio = async () => {
+    if (!audioStarted) {
+      setAudioStarted(true);
+
+      const microphoneTrack = await AgoraRTC.createMicrophoneAudioTrack();
+      await client.publish(microphoneTrack);
+    }
   };
 
   const stopVideo = () => {
     if (videoStarted) {
       localStream.current.cameraTrack.close();
+      client.unpublish(localStream.current.cameraTrack);
+      localStream.current.cameraTrack = null;
+      setVideoStarted(false);
     }
-    setVideoStarted(false);
-  };
-
-  const startScreenShare = async () => {
-    if (videoStarted)
-      localStream.current.cameraTrack.close();
-    setScreenShareStarted(true);
-
-    const screenTrack = await AgoraRTC.createScreenVideoTrack();
-    // await client.publish(screenTrack);
-
-    screenTrack.play('local-video');
-
-    localStream.current = { screenTrack };
-
-    // client.on('user-published', async (user, mediaType) => {
-    //   if (mediaType === 'video') {
-    //     await client.subscribe(user, mediaType);
-    //     const remoteVideoTrack = user.videoTrack;
-    //     remoteVideoTrack.play(screenShareRef.current);
-    //   }
-    // });
   };
 
   const stopScreenShare = () => {
     if (screenShareStarted) {
       localStream.current.screenTrack.close();
+      client.unpublish(localStream.current.screenTrack);
+      localStream.current.screenTrack = null;
+      setScreenShareStarted(false);
     }
-    setScreenShareStarted(false);
-  };
-
-  const startAudio = async () => {
-    setAudioStarted(true);
-    // const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
-    // await client.join(agoraAppId, 'stream', null);
-
-    const microphoneTrack = await AgoraRTC.createMicrophoneAudioTrack();
-    await client.publish(microphoneTrack);
-
-    client.on('user-published', async (user, mediaType) => {
-      if (mediaType === 'audio') {
-        await client.subscribe(user, mediaType);
-        const remoteAudioTrack = user.audioTrack;
-        remoteAudioTrack.play(remoteVideoRef.current);
-      }
-    });
   };
 
   const stopAudio = () => {
     if (audioStarted) {
-      //
+      client.unpublish(localStream.current.microphoneTrack);
+      setAudioStarted(false);
     }
-    setAudioStarted(false);
   };
 
   const stopAgoraStream = () => {
-    if (localStream.current) {
-      localStream.current.cameraTrack.close();
-      localStream.current.screenTrack.close();
-      localStream.current.microphoneTrack.close();
-    }
+    stopVideo();
+    stopScreenShare();
+    stopAudio();
   };
 
   const connectToSignalingServer = () => {
@@ -157,13 +156,6 @@ const Stream = () => {
 
   return (
     <div className='stream-body'>
-      {/* <div id="local-video" style={{ width: '320px', height: '240px', border: '1px solid #ccc', marginBottom: '10px' }}></div>
-      <div id="share-screen" style={{ width: '320px', height: '240px', border: '1px solid #ccc', marginBottom: '10px' }}></div>
-      <video ref={remoteVideoRef} style={{ width: '320px', height: '240px', border: '1px solid #ccc', marginBottom: '10px' }}></video>
-      <video ref={screenShareRef} id="share-screen" style={{ width: '320px', height: '240px', border: '1px solid #ccc', marginBottom: '10px' }}></video>
-      <button onClick={startVideo}>Start Video</button>
-      <button onClick={startScreenShare}>Start Screen Share</button>
-      <button onClick={startAudio}>Start Audio</button> */}
       <div className='videoBox' id="local-video"></div>
       <div className='stream-btns'>
         {!audioStarted && <button className='sxbtn' onClick={startAudio}><FontAwesomeIcon icon={faMicrophoneSlash} /></button>}
@@ -171,9 +163,11 @@ const Stream = () => {
         {!videoStarted && <button className='sxbtn' onClick={startVideo}><FontAwesomeIcon icon={faVideoSlash} /></button>}
         {videoStarted && <button className='sbtn' onClick={stopVideo}><FontAwesomeIcon icon={faVideo} /></button>}
         {!screenShareStarted && <button className='sbtn' onClick={startScreenShare}><FontAwesomeIcon icon={faSquarePlus} /></button>}
+        <video ref={remoteVideoRef} style={{ width: '320px', height: '240px', border: '1px solid #ccc', marginBottom: '10px' }}></video>
+        <video ref={screenShareRef} id="share-screen" style={{ width: '320px', height: '240px', border: '1px solid #ccc', marginBottom: '10px' }}></video>
       </div>
     </div>
   );
-}
+};
 
 export default Stream;
