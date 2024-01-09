c635df5633c26e05e72e66ff263709450578f0cf
Niladri Adhikary <rickyrickado14@gmail.com>
2024-01-09 06:59:47 +0530

    clearing some warning and dependencies

--- client/src/About.js
+++ client/src/About.js
@@ -1,10 +1,8 @@
-import React, { useEffect, useState, useRef} from 'react';
+import React, { useEffect, useRef} from 'react';
 import './About.css';
 import './index.css';
-import Community from './images/community-nobg.png';
 import Experience from './images/experience-nobg.png';
 import Expert from './images/expert-nobg.png';
-import Technology from './images/technology-nobg.png';
 
 function About()
 {
@@ -114,7 +112,7 @@ function About()
 
         <div className='about_blocks'>
             <div className='expert'>
-                <img src={Expert} ref={expertImageRef}/>
+                <img src={Expert} ref={expertImageRef} alt="expert" />
                 <div className='expert_description' ref={expertDescriptionRef}>
                     <h2>Our Team of Experts</h2>
                     <p>We take pride in our team of experts who are passionate about education and dedicated to providing the highest quality learning experience. Our instructors are industry professionals, subject matter experts, and experienced educators who bring their wealth of knowledge and practical insights into the courses they teach.</p>
@@ -126,7 +124,7 @@ function About()
                     <h2>Seamless User Experience</h2>
                     <p>At Prathamik, we are committed to providing a seamless learning experience from start to finish. Our user-friendly platform is designed with intuitive navigation and clear instructions, making it easy for you to navigate through courses, access resources, and track your progress. We understand that your time is valuable, so we strive to.</p>
                 </div>
-                <img src={Experience} ref={experienceImageRef}/>
+                <img src={Experience} ref={experienceImageRef} alt="experience"/>
             </div>
         </div>
 
